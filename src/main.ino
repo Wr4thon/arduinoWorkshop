263f36e34f74030ee4b119edd3878d518e29c736
Johannes Kolata <jko@fino.digital>
2019-10-17 19:43:54 +0200

    some changes to flashing behavior

--- src/main.ino
+++ src/main.ino
@@ -1,44 +1,104 @@
 #include <sketch.h>
 
+uint8_t c1[1]{LED_DUAL_GREEN};
+uint8_t c2[2]{LED_DUAL_RED, LED_DUAL_GREEN};
+uint8_t c3[1]{LED_DUAL_RED};
+
+struct cycleTemplate
+{
+  uint8_t mainPin;
+  uint8_t *flashPins;
+  uint8_t flashPinsAmount;
+  String name;
+
+  cycleTemplate(uint8_t mP, uint8_t *fP, uint8_t fPA, String n)
+  {
+    mainPin = mP;
+    flashPins = fP;
+    flashPinsAmount = fPA;
+    name = n;
+  }
+} cT0(LED_GREEN, c1, 1, "GREEN"), cT1(LED_YELLOW, c2, 2, "YELLOW"), cT2(LED_RED, c3, 1, "RED");
+
+cycleTemplate *cycles[3];
+uint8_t c = - 1;
+uint8_t turns = 3;
+
 void setup()
 {
+  Serial.begin(9600);
+  
   pinMode(LED_GREEN, OUTPUT);
   pinMode(LED_YELLOW, OUTPUT);
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_DUAL_GREEN, OUTPUT);
   pinMode(LED_DUAL_RED, OUTPUT);
-  Serial.begin(9600);
+
+  cycles[0] = &cT0;
+  cycles[1] = &cT1;
+  cycles[2] = &cT2;
 }
 
 void loop(void)
 {
-  cycle(LED_GREEN, LED_DUAL_RED, "GREEN");
-  cycle(LED_YELLOW, LED_DUAL_RED, "YELLOW");
-  cycle(LED_RED, LED_DUAL_GREEN, "RED");
+  c++;
+  if (c >= turns)
+  {
+    c = 0;
+  }
+  cycle(cycles[c]);
 }
 
-unsigned long getDel() {
+unsigned long getDelay()
+{
   return map(analogRead(POT), 0, 1023, 2, 20);
 }
 
-void cycle(uint8_t pin, uint8_t flashPin, String name)
+void cycle(cycleTemplate *ct)
 {
-  Serial.print(name);
+  Serial.print(ct->name);
   Serial.println(" UP");
-  up(pin);
-  Serial.print(name);
+  up(ct->mainPin);
+  Serial.print(ct->name);
   Serial.println(" DOWN");
-  down(pin);
-  flash(flashPin);
+  down(ct->mainPin);
+  flash(ct->flashPins, ct->flashPinsAmount);
 }
 
-void flash(uint8_t pin)
+void flash(uint8_t *pins, uint8_t amount, uint8_t level)
 {
+  for (int i = 0; i < amount; i++)
+  {
+    uint8_t pin = pins[i];
+    Serial.print(pin);
+    Serial.print(" ");
+    Serial.print(level);
+    digitalWrite(pin, level);
+    Serial.print(" ");
+  }
+  
+  Serial.println();
+}
+
+void flash(uint8_t *pins, uint8_t amount)
+{
+  Serial.print("flash '");
+  Serial.print(amount);
+  Serial.print("' pins - ");
+  Serial.print(pins[0]);
+
+  if(amount == 2) {
+    Serial.print(" and ");
+    Serial.print(pins[1]);
+  }
+
+  Serial.println();
+
   for (int i = 0; i < 2; i++)
   {
-    digitalWrite(pin, HIGH);
+    flash(pins, amount, HIGH);
     delay(50);
-    digitalWrite(pin, LOW);
+    flash(pins, amount, LOW);
     delay(50);
   }
 }
@@ -48,7 +108,7 @@ void up(uint8_t pin)
   for (int i = 0; i < 128; i++)
   {
     analogWrite(pin, i);
-    delay(getDel());
+    delay(getDelay());
   }
 }
 
@@ -57,6 +117,6 @@ void down(uint8_t pin)
   for (int i = 128; i >= 0; i--)
   {
     analogWrite(pin, i);
-    delay(getDel());
+    delay(getDelay());
   }
 }
